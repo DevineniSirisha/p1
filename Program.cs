4c814e1cd524b84e00f413a9cd030e8815f09165
Devineni,Sai Sirisha <S531367@nwmissouri.edu>
2019-01-20 18:36:12 -0600

    running program with validations

--- Program.cs
+++ Program.cs
@@ -4,43 +4,97 @@ namespace p1
 {
     class Program
     {
+        const decimal FOOD_COST = 200m;
+        const decimal TRAVEL_COST = 400m;
         static void Main(string[] args)
         {
-            string name="";
-            string jobname="";
-            char EXIT='q';
-            decimal laborCost=0.0m;
-            int days=0;
-            double hours=0.0;
-            decimal acCost=0.0m;
-            decimal FOOD_COST=200m;
-            decimal TRAVEL_COST=400m;
-            bool includeFood=false;
-            bool includeTravel=false;
+            char exit = 'c';
+            while (exit != 'q')
+            {
+                string name = "";
+                string jobName = "";
+                decimal laborCost = 0.0m;
+                int days = 0;
+                int hours = 0;
+                decimal acCost = 0.0m;
+                bool includeFood = false;
+                bool includeTravel = false;
+                while (name == "")
+                {
+                    Console.WriteLine("Hello User! Please enter your name: ");
+                    name = Console.ReadLine();
 
-            Console.WriteLine("Hello User! Please enter your name: ");
-            name=Console.ReadLine();
-            Console.WriteLine("Please enter the job name");
-            jobname=Console.ReadLine();
-            Console.WriteLine("Enter the cost of labor");
-            laborCost=Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("Enter number of hours: ");
-            hours=Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enetr number of days: ");
-            days=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter cost of accomodation :");
-            acCost=Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("Do you want to include food cost ?");
-            includeFood=Convert.ToBoolean(Console.ReadLine());
-            Console.WriteLine("Do you want to include travel cost ?");
-            includeTravel=Convert.ToBoolean(Console.ReadLine());
-            
+                }
+                while (jobName == "")
+                {
+                    Console.WriteLine("Please enter the job name");
+                    jobName = Console.ReadLine();
+                }
+
+                while (laborCost == 0.0m)
+                {
+                    Console.WriteLine($"Enter the cost of labor per hour: ");
+                    laborCost = Convert.ToDecimal(Console.ReadLine());
+                }
+
+                while (hours == 0.0)
+                {
+                    Console.WriteLine($"Enter number of hours per day: ");
+                    hours = Convert.ToInt32(Console.ReadLine());
+                }
+                while (days == 0)
+                {
+                    Console.WriteLine($"Enter number of days: ");
+                    days = Convert.ToInt32(Console.ReadLine());
+                }
+                while (acCost == 0.0m)
+                {
+                    Console.WriteLine($"Enter cost of accomodation :");
+                    acCost = Convert.ToDecimal(Console.ReadLine());
+                }
+                Console.WriteLine($"Do you want to include food cost ?(yes/no)");
+                var ans = Console.ReadLine();
+                if (ans.ToLower() == "yes")
+                {
+                    includeFood = true;
+                }
+                else
+                {
+                    includeFood = false;
+                }
+                Console.WriteLine($"Do you want to include travel cost ?(yes/no)");
+                var a = Console.ReadLine();
+                switch (a.ToLower())
+                {
+                    case "yes":
+                        includeTravel = true;
+                        break;
+                    case "no":
+                        includeTravel = false;
+                        break;
+                    default:
+                        includeTravel = false;
+                        break;
+
+                }
+                var res = calEstimate(laborCost, days, hours, acCost, includeFood, includeTravel);
+                Console.WriteLine($"Hello {name}, your job {jobName} costs you {res} ");
 
 
+                Console.WriteLine($"Enter q to quit or c to continue :");
+                exit = Convert.ToChar(Console.ReadLine());
+                if (exit == 'q')
+                {
+                    Console.WriteLine($"GoodBye! Have a nice day !!");
+                    break;
+                }
+            }
 
-
-
-
+        }
+        static decimal calEstimate(decimal laborCost, int days, int hours, decimal acCost, bool includeFood, bool includeTravel)
+        {
+            var num = days * hours;
+            return days * hours * (laborCost) + days * (acCost + FOOD_COST + TRAVEL_COST);
 
         }
     }
